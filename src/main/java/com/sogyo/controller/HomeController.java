b3e05576c36342223a0a1fb4cf9493436225e9ba
Bart Wolfs <bart.wolfs@ing.nl>
2016-10-21 11:06:16 +0200

    Initial commit

---
+++ src/main/java/com/sogyo/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.sogyo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by ep53rn on 21-10-2016.
+ */
+@Controller
+public class HomeController {
+    @RequestMapping
+}
